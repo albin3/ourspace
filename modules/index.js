e79bf301154e538ad7919f118d9a401ff6dfcbc7
zeng <zeng@localhost.localdomain>
2014-02-22 21:33:19 +0800

    修改模块名称及加载模块方法（自动检测当前目录模块并加载）

--- modules/index.js
+++ modules/index.js
@@ -1,16 +1,22 @@
 // modules 模块，是所有模块的对外接口
+var fs = require('fs');
 
 // 引入子模块
 var account = require('./account');
-var userself = require('./userself');
+var userself = require('./userdata');
 var hall = require('./hall');
 
+var modulespath = "./modules/";
 var m = {};
-// 将子模块挂在到m下方
+// 检测当前目录下的模块，并依此加载
 m.registers = function (app) {
-  account.register(app);
-  userself.register(app);
-  hall.register(app);
+  var result = [], files = fs.readdirSync(modulespath);
+  files.forEach(function(file) {
+    stat = fs.lstatSync(modulespath + "/" + file);
+    if (stat !== undefined && stat.isDirectory()) {
+      require("./" + file).register(app);
+    }
+  });
 };
 
 // 将m作为对外接口
