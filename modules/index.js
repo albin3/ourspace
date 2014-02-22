1c0ccd6d25addf7ad8b068be27b96e0710f3d38e
zeng <zeng@localhost.localdomain>
2014-02-22 21:38:50 +0800

    修改模块加载方式

--- modules/index.js
+++ modules/index.js
@@ -1,16 +1,11 @@
 // modules 模块，是所有模块的对外接口
 var fs = require('fs');
 
-// 引入子模块
-var account = require('./account');
-var userself = require('./userdata');
-var hall = require('./hall');
-
 var modulespath = "./modules/";
 var m = {};
 // 检测当前目录下的模块，并依此加载
 m.registers = function (app) {
-  var result = [], files = fs.readdirSync(modulespath);
+  var files = fs.readdirSync(modulespath);
   files.forEach(function(file) {
     stat = fs.lstatSync(modulespath + "/" + file);
     if (stat !== undefined && stat.isDirectory()) {
@@ -21,4 +16,3 @@ m.registers = function (app) {
 
 // 将m作为对外接口
 module.exports = m;
-
