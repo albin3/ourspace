8266e16174523a370fe59f2edfdfb4cf7a4a484c
Albin <albin@louding.com>
2014-02-24 15:59:32 +0800

    增加about页面

---
+++ modules/about/index.js
@@ -0,0 +1,9 @@
+// about 模块，信息大厅, 显示所有人发的状态
+var view = require('./view');
+
+
+exports.register = function (app) {
+  // TODO: about module.
+  app.get('/about', view.about);
+};
+
---
+++ modules/about/view.js
@@ -0,0 +1,6 @@
+// about view.js
+
+
+exports.about = function(req, res) {
+  res.render('about/about',{});
+}
