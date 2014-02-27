a6dfaf2b25fd821fc254761e5f5970f85cfc8c3a
Albin <albin@louding.com>
2014-02-27 19:38:56 +0800

    aaa

--- modules/userdata/index.js
+++ modules/userdata/index.js
@@ -1,6 +1,8 @@
 // userself 模块,用户信息修改、头像更改
+var view = require('./view');
 
 exports.register = function (app) {
   // TODO: add get, post.
+  app.get('/userdata', view.userdata);
 };
 
---
+++ modules/userdata/view.js
@@ -0,0 +1,5 @@
+// Copyright © 2014 Albin. All Rights Reserved.
+
+exports.userdata = function (req, res) {
+  res.render('userdata/userdata', {});
+};
